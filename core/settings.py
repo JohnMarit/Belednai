dd75451a3f6b877a3449b544af4ddb421cab6d3b
JohnMarit <thuchabraham42@gmail.com>
2025-03-22 10:45:48 +0200

    google loin deleted well

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.home',  # Enable the inner home (home)
-    
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -161,6 +163,8 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #############################################################
+
+
 #############################################################
 
 
